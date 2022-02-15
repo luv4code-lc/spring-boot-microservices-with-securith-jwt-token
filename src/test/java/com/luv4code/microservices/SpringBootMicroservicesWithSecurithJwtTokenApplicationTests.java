128a79d55731ad7be388db6c6b3529454608d959
luv4code-lc <madhav.p.akv@gmail.com>
2022-02-15 08:17:15 +0530

    adding project skeleton

---
+++ src/test/java/com/luv4code/microservices/SpringBootMicroservicesWithSecurithJwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv4code.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesWithSecurithJwtTokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
